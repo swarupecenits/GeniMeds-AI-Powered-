274eb77a636c66081a6429de7eb0cc1934aabe43
swarupmsft <t-schanda@microsoft.com>
2025-06-20 15:41:26 +0530

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
 
 function App() {
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-         Hello world!
-      </h1>
-    </>
+    <Router>
+      <Navbar />
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
