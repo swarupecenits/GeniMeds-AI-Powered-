8a9446c518364f92d0d3faf7b7e487b7a1cb63d9
swarupmsft <t-schanda@microsoft.com>
2025-06-20 15:47:49 +0530

    Added router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     <nav className="p-4 bg-gray-100 shadow-md mb-6">
       <ul className="flex space-x-6">
         <li><Link to="/" className="text-blue-600 font-medium">Home</Link></li>
-        <li><Link to="/profile" className="text-blue-600 font-medium">Profile</Link></li>
         <li><Link to="/about" className="text-blue-600 font-medium">About</Link></li>
+        <li><Link to="/profile" className="text-blue-600 font-medium">Profile</Link></li>
       </ul>
     </nav>
   )
