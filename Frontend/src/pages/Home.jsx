8cb98f28fb7fdd9b6eec6d7b33e8ca0eaf7586e5
swarupmsft <t-schanda@microsoft.com>
2025-06-24 15:48:54 +0530

    Updates

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -14,9 +14,13 @@ const Home = () => {
                             <div class="relative inline-flex mt-10 group">
                                 <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
 
-                                <a href="#" title="" class="z-50 relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" role="button">
+                                <Link
+                                    to="/chat"
+                                    className="z-50 relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                                    role="button"
+                                >
                                     Let's Chat with Our AI Assistant
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
