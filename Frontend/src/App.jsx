cb2f16b5f775f5be3fa2e3ec4f86e8411cf43e67
swarupmsft <t-schanda@microsoft.com>
2025-06-25 12:56:27 +0530

    Updates

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavbarDefault from './components/Navbar';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
@@ -8,21 +8,31 @@ import RegisterForm from './components/RegisterForm';
 import AiChat from './pages/AiChat';
 import Contact from './pages/Contact';
 import Medicine from './pages/Medicine';
+import Footer from './components/Footer';
+
+function AppContent() {
+  const location = useLocation();
+  return (
+    <>
+      <NavbarDefault />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/chat" element={<AiChat />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/medicine" element={<Medicine />} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+      {location.pathname !== '/chat' && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <NavbarDefault />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/chat" element={<AiChat />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/medicine" element={<Medicine />} />
-          <Route path="/register" element={<RegisterForm />} />
-
-        </Routes>
+      <AppContent />
     </Router>
   );
 }
