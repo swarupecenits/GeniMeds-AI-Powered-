2558b99c8aa071968f340f7c014e258176744059
Rashmi <rashmiraj2626@gmail.com>
2025-06-25 23:37:09 +0530

    Login integrated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,9 +8,8 @@ import RegisterForm from './components/RegisterForm';
 import AiChat from './pages/AiChat';
 import Contact from './pages/Contact';
 import Medicine from './pages/Medicine';
-import Product from './pages/Product';
 import Footer from './components/Footer';
-
+import ProtectedRoute from './components/ProtectedRoute';
 function AppContent() {
   const location = useLocation();
   const hideLayout = location.pathname === '/login' || location.pathname === '/register';
@@ -18,13 +17,38 @@ function AppContent() {
     <>
       {!hideLayout && <NavbarDefault />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/chat" element={<AiChat />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/about"
+          element={    
+              <About />
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <AiChat />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/medicine" element={<Medicine />} />
-        <Route path="/product/:id" element={<Product />} />
+        <Route
+          path="/medicine"
+          element={
+            <ProtectedRoute>
+              <Medicine />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/register" element={<RegisterForm />} />
       </Routes>
       {!hideLayout && location.pathname !== '/chat' && <Footer />}
